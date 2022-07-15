65a0cd8e39ec8e17b14b799f2f8429e40a6cc955
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-07-15 21:55:00 +0700

    new App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Menu from './component/MenuComponent';
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <Menu/>
       </header>
     </div>
   );
